85facd8f6e3f3293d26526ea8ca05f97f6fed318
Matias Ramirez <matiasgr3009@gmail.com>
2025-11-27 10:33:24 -0300

    roles created

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,11 @@ const userSchema = Schema(
             type: String,
             required: true,
         },
+        role: {
+            type: String,
+            enum: ["admin", "manager", "reader"],
+            default: "reader",
+        },
     },
     {
         timestamps: true,
